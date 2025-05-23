d6f8d3e6c6281883d77db553e717ba947ad340dd
Baim <135421543+asgarindoo@users.noreply.github.com>
2025-05-23 14:06:55 +0700

    chore: remove dummy file and unused dummy syntax

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,7 +1,5 @@
 import express from 'express';
-import dummyContoller from '../controllers/dummy.contoller';
 const router = express.Router();
 
-router.get('/dummy', dummyContoller.dummy)
 
 export default router;
